c6bf9a7d6db428006519c6feb40d3d0dca566582
Nael Kilani <naelkilanii@gmail.com>
2020-07-26 16:45:32 +0300

    2- Project Authentication and Authorization: - logout.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth'
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  user: firebase.User;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private angularFireAuth: AngularFireAuth) { 
+    this.angularFireAuth.authState
+    .subscribe(user => this.user = user);
   }
 
+  logout() {
+    this.angularFireAuth.signOut();
+  }
 }
