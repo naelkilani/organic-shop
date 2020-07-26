6f4bd39ae580578c0c10224578efe24369a7701a
Nael Kilani <naelkilanii@gmail.com>
2020-07-26 19:52:58 +0300

    2- Project Authentication and Authorization: - Redirecting the user after login.

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -18,9 +18,8 @@ export class AuthGuard implements CanActivate {
       if (user) 
         return true;
 
-      this.router.navigate(['/login']);
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
       return false;
-    }));
-    
+    }));   
   }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,14 @@ import * as firebase from 'firebase';
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private angularFireAuth: AngularFireAuth) { 
+  constructor(private angularFireAuth: AngularFireAuth, private route: ActivatedRoute) { 
     this.user$ = angularFireAuth.authState;
   }
 
   login() : void {
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
+
     this.angularFireAuth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
