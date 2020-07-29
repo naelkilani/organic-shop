268827c5cba8f38d26b742e2f787a6e77aa21e1c
Nael Kilani <naelkilanii@gmail.com>
2020-07-29 20:46:14 +0300

    2- Project Product Management: - Searching for product.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,20 +1,33 @@
 import { Product } from './../../models/product';
 import { map } from 'rxjs/operators';
 import { ProductsService } from './../../products.service';
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent {
-  products$: Observable<Product[]>; 
+export class AdminProductsComponent implements OnDestroy{
+  private products: Product[]; 
+  private subscription: Subscription;
+  filteredProducts: Product[];
 
   constructor(private productsService: ProductsService) {
-    this.products$ = this.productsService.getAll()
+    this.subscription = this.productsService.getAll()
     .snapshotChanges()
-    .pipe(map(products => products.map(product => ({ key: product.key, ...product.payload.val()}))));
+    .pipe(map(sps => sps.map(sp => ({ key: sp.key, ...sp.payload.val()}))))
+    .subscribe(products => this.filteredProducts = this.products = products);
    }
+
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())) :
+      this.products;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
